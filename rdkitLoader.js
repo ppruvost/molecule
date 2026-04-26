341f357085581acb439bb8e6605ed6de77b58c10
ppruvost <patrick.pruvost50@gmail.com>
2026-04-26 17:37:08 +0200

    Implement smilesTo3D_RDKit function
    Add function to convert SMILES to 3D structure using RDKit

--- rdkitLoader.js
+++ rdkitLoader.js
@@ -8,3 +8,19 @@ export async function getRDKit() {
     }
     return RDKit;
 }
+
+export async function smilesTo3D_RDKit(smiles) {
+    const RDKit = await getRDKit();
+
+    const mol = RDKit.get_mol(smiles);
+    if (!mol) throw new Error("SMILES invalide");
+
+    mol.AddHs();
+    mol.EmbedMolecule();
+    mol.UFFOptimizeMolecule();
+
+    const sdf = mol.get_molblock();
+    mol.delete?.();
+
+    return sdf;
+}
