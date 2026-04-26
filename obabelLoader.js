1004159c2221de3740508d4dc0a3286aecfb2c72
ppruvost <patrick.pruvost50@gmail.com>
2026-04-26 18:13:11 +0200

    Delete obabelLoader.js

--- obabelLoader.js
+++
@@ -1,9 +0,0 @@
-export async function smilesTo3D(smiles) {
-    const res = await fetch(
-        `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/${encodeURIComponent(smiles)}/SDF`
-    );
-
-    if (!res.ok) throw new Error();
-
-    return await res.text();
-}
